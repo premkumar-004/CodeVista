3dc865758624b95301d8cc269e02d39a7390d8ea
premkumar-004 <premkumarkardale@gmail.com>
2025-02-26 15:30:50 +0530

    Handling Signup and Login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -15,61 +15,93 @@ import {
     TabsList,
     TabsTrigger,
 } from "@/components/ui/tabs"
+import { useState } from "react"
 
 const Login = () => {
+    const [loginInput, setLoginInput] = useState({
+        email: "",
+        password: "",
+    });
+    const [singupInput, setSignupInput] = useState({
+        name: "",
+        email: "",
+        password: "",
+    });
+
+    const changeInputHandler = (e, type) => {
+        const { name, value } = e.target;
+        if (type === "signup") {
+            setSignupInput({
+                ...singupInput,
+                [name]: value,
+            });
+        } else {
+            setLoginInput({
+                ...loginInput,
+                [name]: value,
+            });
+        }
+    }
+
     return (
-        <Tabs defaultValue="account" className="w-[400px]">
-            <TabsList className="grid w-full grid-cols-2">
-                <TabsTrigger value="signup">Signup</TabsTrigger>
-                <TabsTrigger value="login">Login</TabsTrigger>
-            </TabsList>
-            <TabsContent value="signup">
-                <Card>
-                    <CardHeader>
-                        <CardTitle>Signup</CardTitle>
-                        <CardDescription>
-                            Create a new account and click signup when you are done.
-                        </CardDescription>
-                    </CardHeader>
-                    <CardContent className="space-y-2">
-                        <div className="space-y-1">
-                            <Label htmlFor="name">Name</Label>
-                            <Input id="name" defaultValue="Pedro Duarte" />
-                        </div>
-                        <div className="space-y-1">
-                            <Label htmlFor="username">Username</Label>
-                            <Input id="username" defaultValue="@peduarte" />
-                        </div>
-                    </CardContent>
-                    <CardFooter>
-                        <Button>Signup</Button>
-                    </CardFooter>
-                </Card>
-            </TabsContent>
-            <TabsContent value="login">
-                <Card>
-                    <CardHeader>
-                        <CardTitle>Login</CardTitle>
-                        <CardDescription>
-                            Login using your username and password. If you don't have an account, you can create one by clicking the signup tab.
-                        </CardDescription>
-                    </CardHeader>
-                    <CardContent className="space-y-2">
-                        <div className="space-y-1">
-                            <Label htmlFor="current">Current password</Label>
-                            <Input id="current" type="password" />
-                        </div>
-                        <div className="space-y-1">
-                            <Label htmlFor="new">New password</Label>
-                            <Input id="new" type="password" />
-                        </div>
-                    </CardContent>
-                    <CardFooter>
-                        <Button>Login</Button>
-                    </CardFooter>
-                </Card>
-            </TabsContent>
-        </Tabs>
+        <div className="flex justify-center items-center w-full">
+            <Tabs defaultValue="account" className="w-[400px]">
+                <TabsList className="grid w-full grid-cols-2">
+                    <TabsTrigger value="signup">Signup</TabsTrigger>
+                    <TabsTrigger value="login">Login</TabsTrigger>
+                </TabsList>
+                <TabsContent value="signup">
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>Signup</CardTitle>
+                            <CardDescription>
+                                Create a new account and click signup when you are done.
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent className="space-y-2">
+                            <div className="space-y-1">
+                                <Label htmlFor="name">Name</Label>
+                                <Input name="name" value={singupInput.name} onChange={(e) => changeInputHandler(e, "signup")} type="text" placeholder="eg. Prem Kumar" required="true" />
+                            </div>
+                            <div className="space-y-1">
+                                <Label htmlFor="username">Email</Label>
+                                <Input name="email" value={singupInput.email} onChange={(e) => changeInputHandler(e, "signup")} type="email" placeholder="eg. prem@gmail.com" required="true" />
+                            </div>
+                            <div className="space-y-1">
+                                <Label htmlFor="username">Password</Label>
+                                <Input name="password" value={singupInput.password} onChange={(e) => changeInputHandler(e, "signup")} type="password" placeholder="password" required="true" />
+                            </div>
+                        </CardContent>
+                        <CardFooter>
+                            <Button>Signup</Button>
+                        </CardFooter>
+                    </Card>
+                </TabsContent>
+                <TabsContent value="login">
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>Login</CardTitle>
+                            <CardDescription>
+                                Login using your username and password. If you don't have an account, you can create one by clicking the signup tab.
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent className="space-y-2">
+                            <div className="space-y-1">
+                                <Label htmlFor="current">Email</Label>
+                                <Input name="email" value={loginInput.email} onChange={(e) => changeInputHandler(e, "login")} placeholder="eg. prem@gmail.com" type="email" required="true" />
+                            </div>
+                            <div className="space-y-1">
+                                <Label htmlFor="new">Password</Label>
+                                <Input name="password" value={loginInput.password} onChange={(e) => changeInputHandler(e, "login")} placeholder="password" type="password" required="true" />
+                            </div>
+                        </CardContent>
+                        <CardFooter>
+                            <Button>Login</Button>
+                        </CardFooter>
+                    </Card>
+                </TabsContent>
+            </Tabs>
+        </div>
     )
 }
 
