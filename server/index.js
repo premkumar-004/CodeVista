1d31f1cc0c3e94227377b18c0911f1cbfa8d000e
premkumar-004 <premkumarkardale@gmail.com>
2025-03-04 00:06:19 +0530

    errors resolved

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import connectDB from "./database/db.js";
 import userRoute from "./routes/user.route.js";
 dotenv.config({});
 connectDB();
 const PORT = process.env.PORT || 8080;
 const app = express();
+
+//default middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:8080",
+    credentials: true,
+}));
+app.use(express.urlencoded({ extended: true }));
 
 //API
 app.use("/api/v1/user", userRoute);
